42a5b27ed01202222c6d26473c486a9872ffffe0
조용일 <yedn1128@naver.com>
2022-05-05 12:37:42 +0900

    스프링 데이터 페이징 활용2 - CountQuery

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,15 +2,18 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 import java.util.List;
 
@@ -96,7 +99,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
         // 카운트 분리 이득
         // join 의 수를 줄일 수 있는 경우가 있어서 카운트 최적화 가능하다.
-        int total = queryFactory
+        JPAQuery<Member> countQuery = queryFactory
                 .selectFrom(member)
                 .leftJoin(member.team, team)
                 .where(
@@ -104,11 +107,11 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe())
-                )
-                .fetch()
-                .size();
+                );
 
-        return new PageImpl<>(content, pageable, total);
+        // 첫 번째 페이지 또는 마지막 페이지일 경우 카운트 쿼리가 안나감.
+        return PageableExecutionUtils.getPage(content, pageable, () -> countQuery.fetch().size());
+//        return new PageImpl<>(content, pageable, total);
     }
 
     private BooleanExpression usernameEq(String username) {
